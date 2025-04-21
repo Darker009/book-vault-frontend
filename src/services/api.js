cecd4b7343bdb1a78d8e02b9b9011d866a00633b
Darker <bhosalesudarshan9@gmail.com>
2025-04-22 02:37:58 +0530

    Addition: backen Url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from './tokenService';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://book-vault-dzkz.onrender.com/api',
 });
 
 // Request interceptor
